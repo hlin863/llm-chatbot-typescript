6fc40e6495b2e055e7da548791618e44e234e80c
Haocheng Lin <55798830+hlin863@users.noreply.github.com>
2025-05-18 14:38:50 +0100

    Agent Calls -2 18 05 25

--- src/modules/agent/agent.ts
+++ src/modules/agent/agent.ts
@@ -1,34 +1,53 @@
-/* eslint-disable indent */
 import { Embeddings } from "@langchain/core/embeddings";
 import { Neo4jGraph } from "@langchain/community/graphs/neo4j_graph";
-import { ChatPromptTemplate, PromptTemplate } from "@langchain/core/prompts";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { pull } from "langchain/hub";
-import initRephraseChain, {
-  RephraseQuestionInput,
-} from "./chains/rephrase-question.chain";
+import initRephraseChain, { RephraseQuestionInput } from "./chains/rephrase-question.chain";
 import { BaseChatModel } from "langchain/chat_models/base";
 import { RunnablePassthrough } from "@langchain/core/runnables";
 import { getHistory } from "./history";
 import initTools from "./tools";
 import { AgentExecutor, createOpenAIFunctionsAgent } from "langchain/agents";
 
-// tag::function[]
 export default async function initAgent(
   llm: BaseChatModel,
   embeddings: Embeddings,
   graph: Neo4jGraph
 ) {
-  // TODO: Initiate tools
-  // const tools = ...
-  // TODO: Pull the prompt from the hub
-  // const prompt = ...
-  // TODO: Create an agent
-  // const agent = ...
-  // TODO: Create an agent executor
-  // const executor = ...
-  // TODO: Create a rephrase question chain
-  // const rephraseQuestionChain = ...
-  // TODO: Return a runnable passthrough
-  // return ...
+  const tools = await initTools(llm, embeddings, graph);
+
+  const prompt = await pull<ChatPromptTemplate>(
+    "hwchase17/openai-functions-agent"
+  );
+
+  const agent = await createOpenAIFunctionsAgent({
+    llm,
+    tools,
+    prompt
+  });
+
+  const executor = new AgentExecutor({
+    agent,
+    tools,
+    verbose: true
+  });
+
+  const rephraseQuestionChain = await initRephraseChain(llm);
+
+  return RunnablePassthrough.assign<{ input: string; sessionId: string }, any>({
+    history: async (_input, options) => {
+      const sessionId = options?.configurable?.sessionId;
+      if (!sessionId) {
+        throw new Error("Session ID is missing in the configuration.");
+      }
+      const history = await getHistory(sessionId);
+      return history;
+    },
+  })
+    .assign({
+      rephrasedQuestion: (input: RephraseQuestionInput, config: any) =>
+        rephraseQuestionChain.invoke(input, config),
+    })
+    .pipe(executor)
+    .pick("output");
 }
-// end::function[]
